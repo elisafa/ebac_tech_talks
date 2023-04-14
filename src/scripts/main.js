55fe1633d1970e96a2d393b28d2709b154726108
Elisafa <elisafajunior@hotmail.com>
2023-04-14 11:18:50 -0300

    corrigido a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date('Apr 14, 2023 11:14:00')
+const dataDoEvento = new Date('Apr 14, 2024 11:14:00')
 const timeStampDoEvento = dataDoEvento.getTime()
 
 const contaAsHoras = setInterval(function(){

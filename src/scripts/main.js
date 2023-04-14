4f953e64cb7c0dfa6a375d418cad5b584406e7bc
Elisafa <elisafajunior@hotmail.com>
2023-04-14 11:18:01 -0300

    criado a mensagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+const dataDoEvento = new Date('Apr 14, 2023 11:14:00')
+const timeStampDoEvento = dataDoEvento.getTime()
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date()
+    const timeStampAtual = agora.getTime()
+
+    const diaEmMs = 1000 * 60 * 60 * 24
+    const horaEmMs = 1000 * 60 * 60
+    const minutoEmMS = 1000 * 60
+    
+    
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual
+    const diasAteEvento = Math.floor(distanciaAteOEvento / (diaEmMs)) 
+    const horasAteEvento = Math.floor((distanciaAteOEvento % (diaEmMs)) / (horaEmMs))
+    const minutosAteOEvent = Math.floor((distanciaAteOEvento % (horaEmMs)) / (minutoEmMS))
+    const segundoAteEvento = Math.floor((distanciaAteOEvento % minutoEmMS) / 1000)
+
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteOEvent}min ${segundoAteEvento}s`
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras)
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+}, 1000) 
